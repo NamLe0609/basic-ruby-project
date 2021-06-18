d49454eddb8155b3e258b96274537b4ef964b554
Shee <namhle11111@protonmail.com>
2021-06-18 16:45:59 +0100

    Add comments and clean up code

--- substrings.rb
+++ substrings.rb
@@ -1,10 +1,12 @@
 require "pry"
 
 def substrings(string, array)
+    # Make string lowercase as method is case insensitive
     string.downcase!
     substrings_counted = array.reduce(Hash.new(0)) do |substrings, object|
-        object.downcase!
-        #binding.pry
+
+        # Scan for number of instances of object if detected within given string
+        # Then add that number of instances to dictionary with the key of object (:object)
         if string.include?(object)
             substrings[object] += string.scan(/#{object}/).length
         end

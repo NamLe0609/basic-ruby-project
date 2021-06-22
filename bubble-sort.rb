405c7b2b88674972daa68042c18b1d7b9f68de7d
Shee <namhle11111@protonmail.com>
2021-06-22 18:30:06 +0100

    Add initial bubble sort implementation

---
+++ bubble-sort.rb
@@ -0,0 +1,22 @@
+def bubble_sort(array_to_sort)
+    sorted = false
+    until sorted do
+        number_shifted = false
+        (array_to_sort.size - 1).times do |times|
+
+            # If number to the right is smaller, switch their places
+            if array_to_sort[times] > array_to_sort[times + 1]
+                # Use of parallel assignment
+                array_to_sort[times], array_to_sort[times + 1] = array_to_sort[times + 1], array_to_sort[times]
+                number_shifted = true
+            end
+        end
+
+        # If a number has not been shifted, consider the array sorted
+        unless number_shifted
+            sorted = true
+        end
+    end
+    array_to_sort
+end
+
